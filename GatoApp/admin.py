19a750d671e0ef84045694a76cac77b7338ad0a8
Flauvourr <andres.araya780@gmail.com>
2023-09-18 01:04:04 -0300

    Lo mismo de lo anterior

--- GatoApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import pregunta, respuesta
-
-# Register your models here.
-admin.site.register(pregunta)
-admin.site.register(respuesta)
--- GatoApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GatoappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GatoApp'
