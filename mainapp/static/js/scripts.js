c7514063280ec7da4732a5653a5eed42ab4e3899
ユキチ <142609514+Flauvourr@users.noreply.github.com>
2023-09-23 20:12:38 -0300

    Actualización looks de la página

--- mainapp/static/js/scripts.js
+++ mainapp/static/js/scripts.js
@@ -1,29 +1,25 @@
-
 var numDePregunta = 1
-var sigPregunta = 2
-const primerBoton = document.getElementById('comenzar')
 
 function cambiarPregunta() {
     const divContainer = document.getElementById(`pregunta_${numDePregunta}`)
-    const sigDivContainer = document.getElementById(`pregunta_${sigPregunta}`)
-    console.log(divContainer)
-    console.log(sigDivContainer)
-    console.log(numDePregunta)
-    console.log(sigPregunta)
 
-    if (numDePregunta >= 1 && numDePregunta < 15) {
+    if (numDePregunta===1) {
+        const primerBoton = document.getElementById('comenzar')
+        const backgroundStuff = document.getElementById('backgroundStuff')
+    
+        primerBoton.setAttribute('hidden', 'true')
+        divContainer.removeAttribute('hidden')
+        backgroundStuff.removeAttribute('hidden')
+
+    } else if (numDePregunta >= 1 && numDePregunta < 15) {
+        const sigDivContainer = document.getElementById(`pregunta_${numDePregunta + 1}`)
+        
         divContainer.setAttribute('hidden', 'true')
         sigDivContainer.removeAttribute('hidden')
+
     } else {
         document.getElementById('formContainer').submit()
     }
-    numDePregunta++
-    sigPregunta++
-}
 
-function comenzarQuiz () {
-    const divContainer = document.getElementById(`pregunta_${numDePregunta}`)
-    divContainer.removeAttribute('hidden')
-    comenzar.setAttribute('hidden', 'true')
-     
+    numDePregunta++
 }
