1391bef774426f56f9abc86d0d4d2946e0d79ec3
ユキチ <142609514+Flauvourr@users.noreply.github.com>
2023-09-27 21:12:30 -0300

    Comentarios
    - Hay comentarios en la parte de scripts.js
    - Hay comentarios en la parte de main.html

--- mainapp/static/js/scripts.js
+++ mainapp/static/js/scripts.js
@@ -1,6 +1,21 @@
+
+
+
+
+// La función tiene 3 casos posibles:
+//     1.- Si todavía no empieza el Kahoot (hay que usar el botón de comenzar):
+//         En este caso, esconde el botón y muestra el siguiente div con la pregunta
+//         y sus respuestas, además del fondo.
+//     2.- Si ya ha comenzado el Kahoot:
+//         En ese caso, esconde el div con la pregunta y respuestas actuales y muestra
+//         las siguientes.
+//     3.- Si ya ha terminado el quiz:
+//         Hace un Submit del form.
+
 var numDePregunta = 0
 
 function cambiarPregunta() {  
+
     if (numDePregunta===0) {
         const divContainer = document.getElementById(`pregunta_1`)
         const primerBoton = document.getElementById('comenzar')
