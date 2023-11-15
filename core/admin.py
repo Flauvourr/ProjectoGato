89885c75978fcc486bc87f5397fa6b992262cd8b
Calvo38 <mcaballero@usm.cl>
2023-11-14 21:29:45 -0300

    borrado de imperfectos

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Profile
+
 # Register your models here.
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'nivel')
-admin.site.register(Profile, ProfileAdmin)
